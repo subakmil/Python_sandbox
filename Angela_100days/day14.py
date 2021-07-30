d68c1ee2355df4914689d6f5a78dde5c958ca8af
Milos Subak <subakmil@gmail.com>
2021-07-30 22:26:11 +0200

    day14-semi

--- Angela_100days/day14.py
+++ Angela_100days/day14.py
@@ -1,6 +1,21 @@
 # Higher/Lower Game
 import sys
-sys.path.append("C:/Users/Zasilkovna/OneDrive - Packeta/Osobní/VS Code/_Python_sandbox/Art")
-from higher_lower import logo
+import os
+import random
 
+sys.path.append(
+    "C:/Users/Milos Subak/OneDrive/VS Code projects/_Python_sandbox/Art/")
+
+from higher_lower import logo
+from higher_lower_data import data
+
+os.system("cls")
 print(logo)
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+
+if account_a == account_b:
+    account_b = random.choice(data)
+
+print(account_a, account_b)
