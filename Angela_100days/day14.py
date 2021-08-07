d25b303262cc42c666599bcd003dfee046b12308
Milos Subak <subakmil@gmail.com>
2021-08-07 10:47:55 +0200

    higher-lower game completed

--- Angela_100days/day14.py
+++ Angela_100days/day14.py
@@ -6,16 +6,56 @@ import random
 sys.path.append(
     "C:/Users/Milos Subak/OneDrive/VS Code projects/_Python_sandbox/Art/")
 
-from higher_lower import logo
+from higher_lower import logo, vs
 from higher_lower_data import data
 
+
+def format_data(account):
+    """Takes the account data and returns into printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    """Takes the user guess and follower counts and returns if they got it right."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+
+score = 0
+game_should_continue = True
 os.system("cls")
 print(logo)
-
-account_a = random.choice(data)
 account_b = random.choice(data)
 
-if account_a == account_b:
+while game_should_continue:
+
+    account_a = account_b
     account_b = random.choice(data)
 
-print(account_a, account_b)
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}.")
+
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    os.system("cls")
+    print(logo)
+
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}")
