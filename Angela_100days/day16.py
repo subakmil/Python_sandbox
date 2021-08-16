9bb0963936655b5525b12af2eb84f0288dbe1152
Milos Subak <subakmil@gmail.com>
2021-08-16 21:04:45 +0200

    Starting Code for Coffee Machine project 2

--- Angela_100days/day16.py
+++ Angela_100days/day16.py
@@ -10,10 +10,29 @@
 # my_screen.exitonclick()
 
 # from prettytable import PrettyTable
-from prettytable import PrettyTable
+# from prettytable import PrettyTable
 
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l"
 
-print(table)
+# print(table)
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# Documentation for the Coffee Machine Project 2:
+# https://docs.google.com/document/d/e/2PACX-1vTragRHILyj76AvVgpWeOlEaLBXoxPM_43SdEyffIKtOgarj42SoSAsK6LwLAdHQs2qFLGthRZds6ok/pub
+
+
+# TODO1: Print the report
+
+# TODO2: Check resources sufficient
+
+# TODO3: Process coins
+
+# TODO4: Check transaction successful
+
+# TODO5: Make Coffee
