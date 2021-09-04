890ebdd76097b9ee330bd72ccf517395dfdde2c4
Milos Subak <subakmil@gmail.com>
2021-09-04 18:49:44 +0200

    random walk completed

--- Angela_100days/day18_turtle.py
+++ Angela_100days/day18_turtle.py
@@ -5,25 +5,50 @@ tim = Turtle()
 tim.shape("turtle")
 
 
-pace = 90
+# pace = 90
 
 
-def change_color():
-    R = random.random()
-    G = random.random()
-    B = random.random()
+# def change_color():
+#     R = random.random()
+#     G = random.random()
+#     B = random.random()
 
-    tim.color(R, G, B)
+#     tim.color(R, G, B)
 
 
-for _ in range(3, 9):
-    pace += 10
-    angle = 360 / _
-    change_color()
+# for _ in range(3, 9):
+#     pace += 10
+#     angle = 360 / _
+#     change_color()
 
-    for i in range(_):
-        tim.forward(pace)
-        tim.right(angle)
+#     for i in range(_):
+#         tim.forward(pace)
+#         tim.right(angle)
+
+# Random Walk Challenge
+tim.pensize(3)
+tim.speed(0)
+
+choices = ["front", "right", "left", "back"]
+colors = ["black", "dark cyan", "olive", "dark red"]
+progress = 0
+
+
+while progress < 1001:
+    progress += 1
+    to = random.choice(choices)
+    tim.color(random.choice(colors))
+    if to == "front":
+        tim.forward(10)
+    elif to == "right":
+        tim.right(90)
+        tim.forward(10)
+    elif to == "left":
+        tim.left(90)
+        tim.forward(10)
+    else:
+        tim.left(180)
+        tim.forward(10)
 
 
 screen = Screen()
