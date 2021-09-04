a362ed2faeedd72d2dc7a7e43f316204b3b7ec8a
Milos Subak <subakmil@gmail.com>
2021-09-04 18:12:24 +0200

    turtle shapes, change variations

--- Angela_100days/day18_turtle.py
+++ Angela_100days/day18_turtle.py
@@ -1,7 +1,29 @@
 from turtle import Turtle, Screen
+import random
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
+tim = Turtle()
+tim.shape("turtle")
+
+
+pace = 90
+
+
+def change_color():
+    R = random.random()
+    G = random.random()
+    B = random.random()
+
+    tim.color(R, G, B)
+
+
+for _ in range(3, 9):
+    pace += 10
+    angle = 360 / _
+    change_color()
+
+    for i in range(_):
+        tim.forward(pace)
+        tim.right(angle)
 
 
 screen = Screen()
