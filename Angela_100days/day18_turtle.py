1904c87a69deec634e343a642de879d189e53a81
Milos Subak <subakmil@gmail.com>
2021-09-04 19:11:37 +0200

    spirograph challenge complete

--- Angela_100days/day18_turtle.py
+++ Angela_100days/day18_turtle.py
@@ -8,12 +8,12 @@ tim.shape("turtle")
 # pace = 90
 
 
-# def change_color():
-#     R = random.random()
-#     G = random.random()
-#     B = random.random()
+def change_color():
+    R = random.random()
+    G = random.random()
+    B = random.random()
 
-#     tim.color(R, G, B)
+    tim.color(R, G, B)
 
 
 # for _ in range(3, 9):
@@ -25,30 +25,42 @@ tim.shape("turtle")
 #         tim.forward(pace)
 #         tim.right(angle)
 
-# Random Walk Challenge
-tim.pensize(3)
+# # Random Walk Challenge
+# tim.pensize(5)
+# tim.speed(0)
+
+# choices = ["front", "right", "left", "back"]
+# colors = ["black", "dark cyan", "olive", "dark red"]
+# progress = 0
+
+
+# while progress < 201:
+#     progress += 1
+#     to = random.choice(choices)
+#     change_color()
+#     if to == "front":
+#         tim.forward(15)
+#     elif to == "right":
+#         tim.right(90)
+#         tim.forward(15)
+#     elif to == "left":
+#         tim.left(90)
+#         tim.forward(15)
+#     else:
+#         tim.left(180)
+#         tim.forward(15)
+
+# Spirograph challenge
+
+angle = 0
 tim.speed(0)
 
-choices = ["front", "right", "left", "back"]
-colors = ["black", "dark cyan", "olive", "dark red"]
-progress = 0
+while angle < 360:
+    change_color()
+    tim.circle(150)
 
-
-while progress < 1001:
-    progress += 1
-    to = random.choice(choices)
-    tim.color(random.choice(colors))
-    if to == "front":
-        tim.forward(10)
-    elif to == "right":
-        tim.right(90)
-        tim.forward(10)
-    elif to == "left":
-        tim.left(90)
-        tim.forward(10)
-    else:
-        tim.left(180)
-        tim.forward(10)
+    angle += 5
+    tim.setheading(angle)
 
 
 screen = Screen()
