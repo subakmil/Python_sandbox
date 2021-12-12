ada52b7607e0914d18faa630744346460a497721
Milos Subak <subakmil@gmail.com>
2021-12-12 21:27:35 +0100

    file system manipulating start

---
+++ Angela_100days/day24_main.py
@@ -0,0 +1,6 @@
+# with open("Angela_100days/my_file.txt") as my_file:
+#     contents = my_file.read()
+    # print(contents)
+
+with open("Angela_100days/new_file.txt", mode="w") as my_file:
+    my_file.write("My Name is Milos, bro")
