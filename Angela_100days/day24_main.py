880420c9851e11595bd2a9d053f3977ff7c0c35b
Milos Subak <subakmil@gmail.com>
2022-05-08 18:37:35 +0200

    renewed day24

--- Angela_100days/day24_main.py
+++ Angela_100days/day24_main.py
@@ -1,6 +1,12 @@
 # with open("Angela_100days/my_file.txt") as my_file:
 #     contents = my_file.read()
-    # print(contents)
+#     print(contents)
 
-with open("Angela_100days/new_file.txt", mode="w") as my_file:
-    my_file.write("My Name is Milos, bro")
+# with open("Angela_100days/new_file.txt", mode="w") as my_file:
+#     my_file.write("My Name is Milos, bro")
+
+# with open("Angela_100days/my_file.txt", mode="a") as file:
+#     file.write("\nDneska bol pražský maratón.")
+    
+with open("my_file.txt", mode="w") as file:
+    file.write("\nAchjo.. nechce sa mi do psej matere..")
